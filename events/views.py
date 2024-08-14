a83ec5580c4419113e0d36fd910af1319251d340
amgaina <amgain.abhishek567@gmal.com>
2024-08-14 00:15:40 -0500

    Added calendar on the page

--- events/views.py
+++ events/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html", {})
+def home(request, month, year):
+    name = "Abhishek"
+    month = month.capitalize()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+    now = datetime.now()
+    current_year = now.year
+    time = now.strftime("%I:%M %p")
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(year, month_number)
+
+    return render(request, "home.html", {
+        "name": name,
+        "month": month,
+        "year": year,
+        "cal": cal,
+        "current_year": current_year,
+        "current_time":time
+    })
